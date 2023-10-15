11095ffc10356d78f9ff8f603f641a1ee7308688
webMasterMrBin <songlibin1994@gmail.com>
2023-10-15 22:52:21 +0800

    11

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState, Suspense } from 'react';
 import Cookies from 'js-cookie';
-import { Input } from 'antd';
+import { Input, Button } from 'antd';
 import { testModule } from './index.module.css';
 
 const Demo: FC = () => {
@@ -62,18 +62,18 @@ const Demo: FC = () => {
       <Input placeholder="555" size="large" />
       <div>
         <span>username</span>
-        <input value={username} type="text" onChange={e => setUsername(e.target.value)} />
+        <Input value={username} onChange={e => setUsername(e.target.value)} />
       </div>
       <div>
         <span className={`text-blue-700 ml-2 ${testModule}`}>password</span>
-        <input type="text" onChange={e => setPassword(e.target.value)} />
+        <Input value={password} onChange={e => setPassword(e.target.value)} />
       </div>
 
-      <button onClick={handleSignUp}>注册</button>
-      <button onClick={handleSignIn}>登录</button>
+      <Button onClick={handleSignUp}>注册</Button>
+      <Button onClick={handleSignIn}>登录</Button>
 
-      <button onClick={handleAuth}>登录成功后发起其他请求 校验登录token状态</button>
-      <button onClick={handleTestSession}>test session</button>
+      <Button onClick={handleAuth}>登录成功后发起其他请求 校验登录token状态</Button>
+      <Button onClick={handleTestSession}>test session</Button>
     </div>
   );
 };
